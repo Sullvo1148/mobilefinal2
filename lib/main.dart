a200547762d2bc3bff7e63126e28e368a4dcd071
Sullvo1148 <32890297+Sullvo1148@users.noreply.github.com>
2019-05-25 11:10:22 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,7 @@ import './ui/friend.dart';
 
 
 void main() => runApp(MyApp());
-// const MaterialColor myColor = const MaterialColor(
-//   0xff64b5f6,
-//   const <int, Color>{
-//     50: const Color(0xff64b5f6),
-//     100: const Color(0xff64b5f6),
-//     200: const Color(0xff64b5f6),
-//     300: const Color(0xff64b5f6),
-//     400: const Color(0xff64b5f6),
-//     500: const Color(0xff64b5f6),
-//     600: const Color(0xff64b5f6),
-//     700: const Color(0xff64b5f6),
-//     800: const Color(0xff64b5f6),
-//     900: const Color(0xff64b5f6),
-//   },
-// );
+
 
 class MyApp extends StatelessWidget {
   @override
